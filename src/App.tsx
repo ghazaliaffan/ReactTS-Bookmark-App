166199790a9fe57baf79031ff8b578c8c1bee49c
maryamrizwanq <maryamrizwanq@gmail.com>
2022-10-07 11:30:22 +0500

    Frontend for Bookmark App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./components/post/Navbar";
+import Home from "./components/post/Home";
+import Post from "./components/post/Post";
+import Edit from "./components/post/Edit";
+import Create from "./components/post/Create";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className={"container"}>
+        <Switch>
+          <Route path={"/"} element={<Home />} />
+          <Route path={"/post/:postId"} element={<Post />} />
+          <Route path={"/edit/:postId"} element={<Edit />} />
+          <Route path={"/create"} element={<Create />} />
+        </Switch>
+      </div>
     </div>
   );
 }
